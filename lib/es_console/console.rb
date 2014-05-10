b4fc6e658104771bf8d4b95d93d11e58df48b2f6
sent-hil <me@sent-hil.com>
2014-05-10 14:03:37 -0700

    move EsConsole to its own file

---
+++ lib/es_console/console.rb
@@ -0,0 +1,56 @@
+module EsConsole
+  class Console
+    attr_reader :client
+
+    def self.start
+      new.pry
+    end
+
+    def initialize
+      configure_defaults
+      initialize_client
+      configure_pry
+    end
+
+    def url(url=nil)
+      if url
+        @url = url
+        initialize_client
+      end
+
+      @url
+    end
+
+    def stats(opts={})
+      resp = client.indices.stats opts
+
+      {}.tap do |result|
+        resp['indices'].each do |k, v|
+          result[k] = v['primaries']['docs']['count']
+        end
+      end
+    end
+
+    def count(opts={})
+      resp = client.count opts
+      resp['count']
+    end
+
+    private
+
+    def configure_defaults
+      @url = 'http://localhost:9200'
+    end
+
+    def initialize_client
+      @client = Elasticsearch::Client.new url: @url
+    end
+
+    def configure_pry
+      Pry.config.prompt = [
+        proc { "[es]> " },
+        proc { "[es]* " },
+      ]
+    end
+  end
+end
