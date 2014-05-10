25f04a4ceaaea837d8380c28e95c8c77de2886e4
sent-hil <me@sent-hil.com>
2014-05-10 13:26:31 -0700

    allow setting url

--- lib/es_console.rb
+++ lib/es_console.rb
@@ -5,5 +5,18 @@ module EsConsole
     def self.start
       new.pry
     end
+
+    def initialize
+      configure_defaults
+    end
+
+    def url(url=nil)
+      @url = url  if url
+      @url
+    end
+
+    def configure_defaults
+      @url = 'http://localhost:9200'
+    end
   end
 end
--- spec/es_console.rb
+++ spec/es_console.rb
@@ -2,7 +2,24 @@ require_relative 'spec_helper'
 
 describe EsConsole do
   it 'starts console' do
-    EsConsole::Console.any_instance.should_receive(:pry)
+    EsConsole::Console.any_instance.should_receive :pry
     EsConsole::Console.start
+  end
+
+  subject do
+    EsConsole::Console.new
+  end
+
+  context '#url' do
+    it 'has default url' do
+      subject.url.should == 'http://localhost:9200'
+    end
+
+    it 'sets new url' do
+      new_url = 'http://localhost:9201'
+
+      subject.url new_url
+      subject.url.should == new_url
+    end
   end
 end
