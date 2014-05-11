c776d527748d64678ebd8701f7cccebdd8f6797f
sent-hil <me@sent-hil.com>
2014-05-11 00:03:58 -0700

    minore refactoring in def_method

--- lib/es_console/resource.rb
+++ lib/es_console/resource.rb
@@ -12,32 +12,30 @@ module EsConsole
       first_field = opts[:first_field] || :id
 
       if method.is_a? String
-        if method.include?('.')
-          method = method.split('.')
-        end
+        method = method.split('.')
       end
 
       define_method name do |*args|
-        res = client
+        cl = client
 
         if method.is_a? Array
           method[0...method.length-1].each do |meth|
-            res = res.send meth
+            cl = cl.send meth
           end
 
           method = method.last
         end
 
         if args.empty?
-          resp = res.send method, default_args
+          resp = cl.send method, default_args
         elsif args[0].is_a? Hash
-          resp = res.send method, default_args.merge(args[0])
+          resp = cl.send method, default_args.merge(args[0])
         elsif args[1].is_a? Hash
-          resp = res.send method, default_args.merge(
+          resp = cl.send method, default_args.merge(
             {first_field => args[0]}.merge(args[1])
           )
         else
-          resp = res.send method, default_args.merge(
+          resp = cl.send method, default_args.merge(
             {first_field => args[0]}
           )
         end
